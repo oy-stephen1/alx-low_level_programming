59fc14735857898a38b9bfb22eddf91743009fea
Stephen Ofosu <stephenyofosu@gmail.com>
2023-02-18 01:53:33 -0800

    Negative and Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
+#include <stdio.h>
 
 /* betty style doc for function main goes there */
 /**
- * main - This program will assign a random number to the bariable n
- * Return: 0
+ * main - main function
+ *
+ * Return: always 0
  */
 int main(void)
 {
@@ -14,14 +15,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	} else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	} else
-		printf("%d is negative\n", n)		
-	
+		printf("%d is postive\n", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else
+		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
